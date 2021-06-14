d4630039d5d72d53b1f777a5126aabc02388a100
Gimsongsam <hye_tt@naver.com>
2021-06-14 21:23:21 +0900

    first commit

---
+++ js/make.js
@@ -0,0 +1,66 @@
+// $(document).ready(function(){
+    
+//     //******  동작 구성 ******//
+
+//     $("#mandalart ul").addClass("active");
+//     $("#mandalart ul:eq(4)").removeClass();
+
+
+//     const $mandalVal1 = $("#mandalart ul:eq(4) li:eq(0) input")
+//     const $mandalVal2 = $("#mandalart ul:eq(4) li:eq(1) input")
+//     const $mandalVal3 = $("#mandalart ul:eq(4) li:eq(2) input")
+//     const $mandalVal4 = $("#mandalart ul:eq(4) li:eq(3) input")
+//     const $mandalVal5 = $("#mandalart ul:eq(4) li:eq(5) input")
+//     const $mandalVal6 = $("#mandalart ul:eq(4) li:eq(6) input")
+//     const $mandalVal7 = $("#mandalart ul:eq(4) li:eq(7) input")
+//     const $mandalVal8 = $("#mandalart ul:eq(4) li:eq(8) input")
+
+//     $mandalVal1.keyup(() => {
+//         $("#mandalart ul:eq(0) li:eq(4) input").val($mandalVal1.val());
+//         $("#mandalart .wrap ul:eq(0).active").css("opacity","1");
+//         $("#mandalart .wrap ul:eq(0) li input").attr("disabled", false);
+//     });
+//     $mandalVal2.keyup(() => {
+//         $("#mandalart ul:eq(1) li:eq(4) input").val($mandalVal2.val());
+//         $("#mandalart .wrap ul:eq(1).active").css("opacity","1");
+//         $("#mandalart .wrap ul:eq(1) li input").attr("disabled", false);
+//     });
+//     $mandalVal3.keyup(() => {
+//         $("#mandalart ul:eq(2) li:eq(4) input").val($mandalVal3.val());
+//         $("#mandalart .wrap ul:eq(2).active").css("opacity","1");
+//         $("#mandalart .wrap ul:eq(2) li input").attr("disabled", false);
+//     });
+//     $mandalVal4.keyup(() => {
+//         $("#mandalart ul:eq(3) li:eq(4) input").val($mandalVal4.val());
+//         $("#mandalart .wrap ul:eq(3).active").css("opacity","1");
+//         $("#mandalart .wrap ul:eq(3) li input").attr("disabled", false);
+//     });
+//     $mandalVal5.keyup(() => {
+//         $("#mandalart ul:eq(5) li:eq(4) input").val($mandalVal5.val());
+//         $("#mandalart .wrap ul:eq(5).active").css("opacity","1");
+//         $("#mandalart .wrap ul:eq(5) li input").attr("disabled", false);
+//     });
+//     $mandalVal6.keyup(() => {
+//         $("#mandalart ul:eq(6) li:eq(4) input").val($mandalVal6.val());
+//         $("#mandalart .wrap ul:eq(6).active").css("opacity","1");
+//         $("#mandalart .wrap ul:eq(6) li input").attr("disabled", false);
+//     });
+//     $mandalVal7.keyup(() => {
+//         $("#mandalart ul:eq(7) li:eq(4) input").val($mandalVal7.val());
+//         $("#mandalart .wrap ul:eq(7).active").css("opacity","1");
+//         $("#mandalart .wrap ul:eq(7) li input").attr("disabled", false);
+//     });
+//     $mandalVal8.keyup(() => {
+//         $("#mandalart ul:eq(8) li:eq(4) input").val($mandalVal8.val());
+//         $("#mandalart .wrap ul:eq(8).active").css("opacity","1");
+//     });
+
+// });
+
+
+
+
+
+
+
+
